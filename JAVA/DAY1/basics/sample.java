e0aad4f8b3d3a1c45348b377bb2fd1460c0f1f11
venkatabhavaniramyaa <101253610+venkatabhavaniramyaa@users.noreply.github.com>
2022-04-12 12:47:51 +0530

    Create sample.java

---
+++ JAVA/DAY1/basics/sample.java
@@ -0,0 +1,18 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		double a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 2 numbers");
+		a=sc.nextDouble();
+		b=sc.nextDouble();
+		c=a/b;
+		System.out.println("Division = "+c);
+
+	}
+
+}
