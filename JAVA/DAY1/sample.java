d31e252b7e5231b2d10f8269d112199738802664
venkatabhavaniramyaa <101253610+venkatabhavaniramyaa@users.noreply.github.com>
2022-04-12 21:49:27 +0530

    Update sample.java

--- JAVA/DAY1/sample.java
+++ JAVA/DAY1/sample.java
@@ -1,1 +1,19 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		double a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 2 numbers");
+		a=sc.nextDouble();
+		b=sc.nextDouble();
+		c=a/b;
+		System.out.println("Division = "+c);
+
+	}
+
+}
 
